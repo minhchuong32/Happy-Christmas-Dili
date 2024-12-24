6c5ecac671b859de4f66cc3d7222ae2dac79cb73
chuongminh32 <23110187@student.hcmute.edu.vn>
2024-12-24 12:08:28 +0700

    fix text

--- app.js
+++ app.js
@@ -180,7 +180,6 @@ loop();
 
 //----------tree----------
 // MorphSVGPlugin.convertToPath("polygon");
-
       // Khi tài liệu đã sẵn sàng
       document.addEventListener("DOMContentLoaded", function() {
         // Sử dụng MorphSVGPlugin để chuyển đổi polygon thành đường path
@@ -195,7 +194,7 @@ loop();
             ease: "elastic.out(1, 0.5)"
         });
     });
-
+    
 var xmlns = "http://www.w3.org/2000/svg",
     xlinkns = "http://www.w3.org/1999/xlink",
     select = function (s) {
@@ -498,7 +497,7 @@ $(document).ready(function () {
 
     let textTitle = "Gửi Dili ! ";
     let charArrTitle = textTitle.split("");
-    let text = " Merry Christmas! 🎄✨ Chúc mừng bạn đã vượt qua kỳ thi vừa rồi! ...";
+    let text =  " Merry Christmas Dili! 🎄✨ Chúc mừng dili đã vượt qua kỳ thi vừa rồi! Giáng sinh này, hãy dành thời gian để thư giãn và tận hưởng không khí an lành, ấm áp bên những người thân yêu. Hy vọng mùa lễ hội sẽ mang đến cho dili thật nhiều niềm vui, hạnh phúc và tiếp thêm động lực cho những mục tiêu sắp tới. Chúc dili một Giáng sinh tràn đầy yêu thương và một năm mới thành công rực rỡ ^^ 🎅❤️";
     let charArrContent = text.split("");
     
     var currentIndexTitle = 0;
